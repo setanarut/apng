641cb3dc71911f39fb2010efdbba74c4c5d0237d
Barış <duurkak@yandex.com>
2025-04-07 02:10:16 +0300

    paletted image encoding

--- _example/main.go
+++ _example/main.go
@@ -8,31 +8,42 @@ import (
 	"github.com/setanarut/apng"
 )
 
-const FrameCount int = 40
+const FrameCount int = 30
 
 func main() {
-
 	frames := make([]image.Image, FrameCount)
+	// frames2 := make([]image.Image, FrameCount)
+
 	for i := range FrameCount {
-		frames[i] = generateNoiseImage(600, 200)
+		frames[i] = generatePalettedFrames(600, 200)
+		// frames2[i] = generateRGBAFrames(600, 200)
 	}
-	apng.Save("out.png", frames, 3)
+	apng.Save("outPaletted.png", frames, 7)
+	// apng.Save("outRGBA.png", frames2, 7)
 }
 
-func generateNoiseImage(width, height int) *image.RGBA {
+func generateRGBAFrames(width, height int) *image.RGBA {
 	img := image.NewRGBA(image.Rect(0, 0, width, height))
 	for y := range height {
 		for x := range width {
-			col := noisePalette[rand.IntN(4)]
-			img.SetRGBA(x, y, col)
+			c := plt[rand.IntN(3)]
+			img.SetRGBA(x, y, c.(color.RGBA))
+		}
+	}
+	return img
+}
+func generatePalettedFrames(width, height int) *image.Paletted {
+	img := image.NewPaletted(image.Rect(0, 0, width, height), plt)
+	for y := range height {
+		for x := range width {
+			img.SetColorIndex(x, y, uint8(rand.IntN(3)))
 		}
 	}
 	return img
 }
 
-var noisePalette = []color.RGBA{
-	{R: 0, G: 0, B: 0, A: 255},   // Black
-	{R: 255, G: 0, B: 0, A: 255}, // Red
-	{R: 0, G: 255, B: 0, A: 255}, // Green
-	{R: 0, G: 0, B: 255, A: 255}, // Blue
+var plt = []color.Color{
+	color.RGBA{R: 0, G: 0, B: 0, A: 255},       // Black
+	color.RGBA{R: 255, G: 255, B: 255, A: 255}, // White
+	color.RGBA{R: 255, G: 0, B: 255, A: 255},   // White
 }
